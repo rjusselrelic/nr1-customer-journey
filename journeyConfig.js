3a4f4971ebf7856abf8c32dabe76b39a20e100d5
Ryan Jussel <rjussel@newrelic.com>
2020-06-08 19:28:14 -0500

    Added Adobe Sign Config

--- journeyConfig.js
+++ journeyConfig.js
@@ -1,8 +1,8 @@
 const journeyConfig = [
   {
     id: 0,
-    title: 'Demo Journey 1',
-    accountId: 1606862,
+    title: '[Prod] Sign - Customer Journey (Shard)',
+    accountId: 476672,
     funnel: {
       event: 'PageView',
       measure: 'session'
@@ -34,59 +34,72 @@ const journeyConfig = [
     series: [
       {
         id: 0,
-        label: 'All Users',
-        nrqlWhere: "appName = 'WebPortal'",
-        altNrql: {
-          JavaScriptError: " appName = 'WebPortal' "
-        }
+        label: 'All Shards',
+        nrqlWhere: "appName like 'EchoSign-prod%'",
       },
       {
         id: 1,
-        label: 'Columbus',
-        nrqlWhere: "appName = 'WebPortal' and city = 'Columbus' "
+        label: 'North America',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-na%-%'"
       },
       {
         id: 2,
-        label: 'Internet Explorer',
-        nrqlWhere: " appName = 'WebPortal' and userAgentName = 'IE' ",
-        altNrql: {
-          JavaScriptError: " appName = 'WebPortal' and userAgentName = 'IE' "
-        }
+        label: 'Europe',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-eu%-%'"
+      },
+      {
+        id: 3,
+        label: 'India',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-in%-%'"
+      },
+      {
+        id: 4,
+        label: 'Australia',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-au%-%'"
       }
+
     ],
     steps: [
       {
         id: 0,
-        label: 'Homepage',
+        label: 'Home',
         nrqlWhere:
-          "pageUrl = 'http://webportal.telco.nrdemo.com/' OR pageUrl = 'http://webportal.telco.nrdemo.com/index.html'",
+          "pageUrl like '%'",
         altNrql: {
-          JavaScriptError: " requestUri = '/' or requestUri = '/index.html' "
+          JavaScriptError: " requestUri like '%' "
         }
       },
       {
         id: 1,
-        label: 'Plans',
+        label: 'Login',
         nrqlWhere:
-          "pageUrl like 'http://webportal.telco.nrdemo.com/browse/plans%'",
+          "pageUrl like '%login%'",
         altNrql: {
-          JavaScriptError: " requestUri like '/browse/plans%' "
+            JavaScriptError: " requestUri like '%/login' "
         }
       },
       {
         id: 2,
-        label: 'Cart',
-        nrqlWhere: "pageUrl = 'http://webportal.telco.nrdemo.com/shoppingcart'",
+        label: 'Compose',
+        nrqlWhere: "pageUrl like '%compose%'",
         altNrql: {
-          JavaScriptError: " requestUri like '/shoppingcart%' "
+          JavaScriptError: " requestUri like '%/compose' "
         }
       },
       {
         id: 3,
-        label: 'Checkout',
-        nrqlWhere: "pageUrl = 'http://webportal.telco.nrdemo.com/checkout'",
+        label: 'Sign (Agreement)',
+        nrqlWhere: "pageUrl like '%agreements%'",
         altNrql: {
-          JavaScriptError: " requestUri like '/checkout%' "
+          JavaScriptError: " requestUri like '%/agreements' "
+        }
+      },
+      {
+        id: 4,
+        label: 'Send (Agreement)',
+        nrqlWhere: "pageUrl like '%send%'",
+        altNrql: {
+          JavaScriptError: " requestUri like '%/%send' "
         }
       }
     ],
@@ -96,7 +109,7 @@ const journeyConfig = [
         ref: 'clickCount',
         type: 'integer',
         value: {
-          nrql: "SELECT count(*) FROM PageView WHERE appName = 'WebPortal'",
+          nrql: "SELECT count(*) FROM PageView ",
           display: 'integer'
         }
       },
@@ -106,7 +119,7 @@ const journeyConfig = [
         type: 'integer',
         value: {
           nrql:
-            "FROM PageView SELECT uniqueCount(session) WHERE appName = 'WebPortal'",
+            "FROM PageView SELECT uniqueCount(session) ",
           display: 'integer'
         }
       },
@@ -117,7 +130,7 @@ const journeyConfig = [
         value: {
           eventName: 'JavaScriptError',
           nrql:
-            "SELECT count(*) FROM JavaScriptError WHERE appName = 'WebPortal'",
+            "SELECT count(*) FROM JavaScriptError ",
           display: 'integer'
         }
       },
@@ -136,7 +149,7 @@ const journeyConfig = [
         type: 'decimal',
         value: {
           nrql:
-            "FROM PageView SELECT average(duration) WHERE appName = 'WebPortal'",
+            "FROM PageView SELECT average(duration) ",
           display: 'seconds'
         }
       },
@@ -146,142 +159,476 @@ const journeyConfig = [
         type: 'percentile',
         value: {
           nrql:
-            "FROM PageView SELECT percentile(duration, 99) WHERE appName = 'WebPortal'",
+            "FROM PageView SELECT percentile(duration, 99) ",
+          display: 'seconds'
+        }
+      }
+    ]
+  },
+  {
+    id: 1,
+    title: '[Stage] Sign - Customer Journey (Shard)',
+    accountId: 476672,
+    funnel: {
+      event: 'PageView',
+      measure: 'session'
+    },
+    kpis: [
+      {
+        label: 'Error Rate',
+        ref: 'errorRate',
+        value: 3.0,
+        bound: 'higherViolation',
+        description:
+          'If the error rate is higher that 3%, mark that as a notable.'
+      },
+      {
+        label: 'Page views',
+        ref: 'clickCount',
+        value: 5.0,
+        bound: 'percentage',
+        description: 'If the percentage change is plus or minus 10%, flag that.'
+      },
+      {
+        label: 'Page Load Avg.',
+        ref: 'averageDuration',
+        value: 1,
+        bound: 'lowerTarget',
+        description: "We're targeting sub-second load times."
+      }
+    ],
+    series: [
+      {
+        id: 0,
+        label: 'All Shards',
+        nrqlWhere: "appName like 'EchoSign-stage%'",
+      },
+      {
+        id: 1,
+        label: 'N. America (*-na#-dc#)',
+        nrqlWhere: "appName LIKE 'EchoSign-stage-na%-%'"
+      },
+      {
+        id: 2,
+        label: 'Europe (*-eu#-dc#)',
+        nrqlWhere: "appName LIKE 'EchoSign-stage-eu%-%'"
+      },
+      {
+        id: 3,
+        label: 'India (*-in#-dc#)',
+        nrqlWhere: "appName LIKE 'EchoSign-stage-in%-%'"
+      },
+      {
+        id: 4,
+        label: 'Australia (*-au#-dc#)',
+        nrqlWhere: "appName LIKE 'EchoSign-stage-au%-%'"
+      }
+
+    ],
+    steps: [
+      {
+        id: 0,
+        label: 'Home',
+        nrqlWhere:
+          "pageUrl like '%'"
+      },
+      {
+        id: 1,
+        label: 'Login',
+        nrqlWhere:
+          "pageUrl like '%login%'"
+      },
+      {
+        id: 2,
+        label: 'Compose',
+        nrqlWhere: "pageUrl like '%compose%'"
+      },
+      {
+        id: 3,
+        label: 'Sign (Agreement)',
+        nrqlWhere: "pageUrl like '%agreements%'",
+      },
+      {
+        id: 4,
+        label: 'Send (Agreement)',
+        nrqlWhere: "pageUrl like '%send%'",
+      }
+    ],
+    stats: [
+      {
+        label: 'Page views',
+        ref: 'clickCount',
+        type: 'integer',
+        value: {
+          nrql: "SELECT count(*) FROM PageView ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Sessions',
+        ref: 'sessionCount',
+        type: 'integer',
+        value: {
+          nrql:
+            "FROM PageView SELECT uniqueCount(session) ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Error count',
+        ref: 'errorCount',
+        type: 'integer',
+        value: {
+          eventName: 'JavaScriptError',
+          nrql:
+            "SELECT count(*) FROM JavaScriptError ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Error rate',
+        ref: 'errorRate',
+        type: 'decimal',
+        value: {
+          calculation: { rate: ['errorCount', 'clickCount'] },
+          display: 'percentage'
+        }
+      },
+      {
+        label: 'Avg perf',
+        ref: 'averageDuration',
+        type: 'decimal',
+        value: {
+          nrql:
+            "FROM PageView SELECT average(duration) ",
+          display: 'seconds'
+        }
+      },
+      {
+        label: '99th perc',
+        ref: 'nnthPercentile',
+        type: 'percentile',
+        value: {
+          nrql:
+            "FROM PageView SELECT percentile(duration, 99) ",
+          display: 'seconds'
+        }
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: '[Prod] Sign Customer Journey (Browser)',
+    accountId: 476672,
+    funnel: {
+      event: 'PageView',
+      measure: 'session'
+    },
+    kpis: [
+      {
+        label: 'Error Rate',
+        ref: 'errorRate',
+        value: 3.0,
+        bound: 'higherViolation',
+        description:
+          'If the error rate is higher that 3%, mark that as a notable.'
+      },
+      {
+        label: 'Page views',
+        ref: 'clickCount',
+        value: 5.0,
+        bound: 'percentage',
+        description: 'If the percentage change is plus or minus 10%, flag that.'
+      },
+      {
+        label: 'Page Load Avg.',
+        ref: 'averageDuration',
+        value: 1,
+        bound: 'lowerTarget',
+        description: "We're targeting sub-second load times."
+      }
+    ],
+    series: [
+      {
+        id: 0,
+        label: 'Chrome',
+        nrqlWhere: "appName LIKE 'EchoSign-prod%' AND userAgentName = 'Chrome' "
+      },
+      {
+        id: 1,
+        label: 'Internet Explorer',
+        nrqlWhere: "appName LIKE 'EchoSign-prod%' AND userAgentName = 'IE' "
+      },
+      {
+        id: 2,
+        label: 'Microsoft Edge',
+        nrqlWhere: "appName LIKE 'EchoSign-prod%' AND userAgentName = 'Microsoft Edge' "
+      },
+      {
+        id: 3,
+        label: 'Firefox',
+        nrqlWhere: "appName LIKE 'EchoSign-prod%' AND userAgentName = 'Firefox' "
+      },
+      {
+        id: 4,
+        label: 'Safari',
+        nrqlWhere: "appName LIKE 'EchoSign-prod%' AND userAgentName = 'Safari' "
+      }
+
+    ],
+    steps: [
+      {
+        id: 0,
+        label: 'Home',
+        nrqlWhere:
+          "pageUrl like '%admin%'"
+      },
+      {
+        id: 1,
+        label: 'Login',
+        nrqlWhere:
+          "pageUrl like '%login%'"
+      },
+      {
+        id: 2,
+        label: 'Compose',
+        nrqlWhere: "pageUrl like '%compose%'"
+      },
+      {
+        id: 3,
+        label: 'Sign (Agreement)',
+        nrqlWhere: "pageUrl like '%agreements%'",
+      },
+      {
+        id: 4,
+        label: 'Send (Agreement)',
+        nrqlWhere: "pageUrl like '%send%'",
+      }
+    ],
+    stats: [
+      {
+        label: 'Page views',
+        ref: 'clickCount',
+        type: 'integer',
+        value: {
+          nrql: "SELECT count(*) FROM PageView ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Sessions',
+        ref: 'sessionCount',
+        type: 'integer',
+        value: {
+          nrql:
+            "FROM PageView SELECT uniqueCount(session) ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Error count',
+        ref: 'errorCount',
+        type: 'integer',
+        value: {
+          eventName: 'JavaScriptError',
+          nrql:
+            "SELECT count(*) FROM JavaScriptError ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Error rate',
+        ref: 'errorRate',
+        type: 'decimal',
+        value: {
+          calculation: { rate: ['errorCount', 'clickCount'] },
+          display: 'percentage'
+        }
+      },
+      {
+        label: 'Avg perf',
+        ref: 'averageDuration',
+        type: 'decimal',
+        value: {
+          nrql:
+            "FROM PageView SELECT average(duration) ",
+          display: 'seconds'
+        }
+      },
+      {
+        label: '99th perc',
+        ref: 'nnthPercentile',
+        type: 'percentile',
+        value: {
+          nrql:
+            "FROM PageView SELECT percentile(duration, 99) ",
+          display: 'seconds'
+        }
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: '[Prod] Sign - Customer Journey (Shard) DC Only ',
+    accountId: 476672,
+    funnel: {
+      event: 'PageView',
+      measure: 'session'
+    },
+    kpis: [
+      {
+        label: 'Error Rate',
+        ref: 'errorRate',
+        value: 3.0,
+        bound: 'higherViolation',
+        description:
+          'If the error rate is higher that 3%, mark that as a notable.'
+      },
+      {
+        label: 'Page views',
+        ref: 'clickCount',
+        value: 5.0,
+        bound: 'percentage',
+        description: 'If the percentage change is plus or minus 10%, flag that.'
+      },
+      {
+        label: 'Page Load Avg.',
+        ref: 'averageDuration',
+        value: 1,
+        bound: 'lowerTarget',
+        description: "We're targeting sub-second load times."
+      }
+    ],
+    series: [
+      {
+        id: 1,
+        label: 'North America',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-na%-%'"
+      },
+      {
+        id: 2,
+        label: 'Europe',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-eu%-%'"
+      },
+      {
+        id: 3,
+        label: 'India',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-in%-%'"
+      },
+      {
+        id: 4,
+        label: 'Australia',
+        nrqlWhere: "appName LIKE 'EchoSign-prod-au%-%'"
+      }
+
+    ],
+    steps: [
+      {
+        id: 0,
+        label: 'Home',
+        nrqlWhere:
+          "pageUrl like '%'",
+        altNrql: {
+          JavaScriptError: " requestUri like '%' "
+        }
+      },
+      {
+        id: 1,
+        label: 'Login',
+        nrqlWhere:
+          "pageUrl like '%login%'",
+        altNrql: {
+            JavaScriptError: " requestUri like '%/login' "
+        }
+      },
+      {
+        id: 2,
+        label: 'Compose',
+        nrqlWhere: "pageUrl like '%compose%'",
+        altNrql: {
+          JavaScriptError: " requestUri like '%/compose' "
+        }
+      },
+      {
+        id: 3,
+        label: 'Sign (Agreement)',
+        nrqlWhere: "pageUrl like '%agreements%'",
+        altNrql: {
+          JavaScriptError: " requestUri like '%/agreements' "
+        }
+      },
+      {
+        id: 4,
+        label: 'Send (Agreement)',
+        nrqlWhere: "pageUrl like '%send%'",
+        altNrql: {
+          JavaScriptError: " requestUri like '%/%send' "
+        }
+      }
+    ],
+    stats: [
+      {
+        label: 'Page views',
+        ref: 'clickCount',
+        type: 'integer',
+        value: {
+          nrql: "SELECT count(*) FROM PageView ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Sessions',
+        ref: 'sessionCount',
+        type: 'integer',
+        value: {
+          nrql:
+            "FROM PageView SELECT uniqueCount(session) ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Error count',
+        ref: 'errorCount',
+        type: 'integer',
+        value: {
+          eventName: 'JavaScriptError',
+          nrql:
+            "SELECT count(*) FROM JavaScriptError ",
+          display: 'integer'
+        }
+      },
+      {
+        label: 'Error rate',
+        ref: 'errorRate',
+        type: 'decimal',
+        value: {
+          calculation: { rate: ['errorCount', 'clickCount'] },
+          display: 'percentage'
+        }
+      },
+      {
+        label: 'Avg perf',
+        ref: 'averageDuration',
+        type: 'decimal',
+        value: {
+          nrql:
+            "FROM PageView SELECT average(duration) ",
+          display: 'seconds'
+        }
+      },
+      {
+        label: '99th perc',
+        ref: 'nnthPercentile',
+        type: 'percentile',
+        value: {
+          nrql:
+            "FROM PageView SELECT percentile(duration, 99) ",
           display: 'seconds'
         }
       }
     ]
   },
 
-  {
-    id: 1,
-    title: 'Demo Journey 2',
-    accountId: 1606862,
-    funnel: {
-      event: 'PageView',
-      measure: 'session'
-    },
-    series: [
-      {
-        id: 0,
-        label: 'All Users',
-        nrqlWhere: "appName = 'WebPortal'",
-        altNrql: {
-          JavaScriptError: " appName = 'WebPortal' "
-        }
-      },
-      {
-        id: 1,
-        label: 'Columbus',
-        nrqlWhere: "appName = 'WebPortal' and city = 'Columbus' "
-      },
-      {
-        id: 2,
-        label: 'Internet Explorer',
-        nrqlWhere: " appName = 'WebPortal' and userAgentName = 'IE' ",
-        altNrql: {
-          JavaScriptError: " appName = 'WebPortal' and userAgentName = 'IE' "
-        }
-      }
-    ],
-    steps: [
-      {
-        id: 0,
-        label: 'Homepage',
-        nrqlWhere:
-          "pageUrl = 'http://webportal.telco.nrdemo.com/' OR pageUrl = 'http://webportal.telco.nrdemo.com/index.html'",
-        altNrql: {
-          JavaScriptError: " requestUri = '/' or requestUri = '/index.html' "
-        }
-      },
-      {
-        id: 1,
-        label: 'Plans',
-        nrqlWhere:
-          "pageUrl like 'http://webportal.telco.nrdemo.com/browse/plans%'",
-        altNrql: {
-          JavaScriptError: " requestUri like '/browse/plans%' "
-        }
-      },
-      {
-        id: 2,
-        label: 'Cart',
-        nrqlWhere: "pageUrl = 'http://webportal.telco.nrdemo.com/shoppingcart'",
-        altNrql: {
-          JavaScriptError: " requestUri like '/shoppingcart%' "
-        }
-      },
-      {
-        id: 3,
-        label: 'Checkout',
-        nrqlWhere: "pageUrl = 'http://webportal.telco.nrdemo.com/checkout'",
-        altNrql: {
-          JavaScriptError: " requestUri like '/checkout%' "
-        }
-      }
-    ],
-    stats: [
-      {
-        label: 'Page views',
-        ref: 'clickCount',
-        type: 'integer',
-        value: {
-          nrql: "SELECT count(*) FROM PageView WHERE appName = 'WebPortal'",
-          display: 'integer'
-        }
-      },
-      {
-        label: 'Sessions',
-        ref: 'sessionCount',
-        type: 'integer',
-        value: {
-          nrql:
-            "FROM PageView SELECT uniqueCount(session) WHERE appName = 'WebPortal'",
-          display: 'integer'
-        }
-      },
-      {
-        label: 'Error count',
-        ref: 'errorCount',
-        type: 'integer',
-        value: {
-          eventName: 'JavaScriptError',
-          nrql:
-            "SELECT count(*) FROM JavaScriptError WHERE appName = 'WebPortal'",
-          display: 'integer'
-        }
-      },
-      {
-        label: 'Error rate',
-        ref: 'errorRate',
-        type: 'decimal',
-        value: {
-          calculation: { rate: ['errorCount', 'clickCount'] },
-          display: 'percentage'
-        }
-      },
-      {
-        label: 'Avg perf',
-        ref: 'averageDuration',
-        type: 'decimal',
-        value: {
-          nrql:
-            "FROM PageView SELECT average(duration) WHERE appName = 'WebPortal'",
-          display: 'seconds'
-        }
-      },
-      {
-        label: '99th perc',
-        ref: 'nnthPercentile',
-        type: 'percentile',
-        value: {
-          nrql:
-            "FROM PageView SELECT percentile(duration, 99) WHERE appName = 'WebPortal'",
-          display: 'seconds'
-        }
-      }
-    ]
-  }
 ];
 export const getJourneys = () => {
   return journeyConfig;
